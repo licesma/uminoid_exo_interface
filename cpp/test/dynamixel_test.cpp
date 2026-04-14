aee79ab4899c5053480866a574b43a71e866fb33
Esteban Martinez <licesma@gmail.com>
2026-04-14 14:37:14 -0700

    Add velocity to inspire retarget

--- cpp/test/dynamixel_test.cpp
+++ cpp/test/dynamixel_test.cpp
@@ -2,10 +2,11 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <optional>
 
 int main(int argc, char const* argv[]) {
   const std::string left_device = argc >= 2 ? argv[1] : "/dev/ttyUSB0";
-  const std::string right_device = argc >= 3 ? argv[2] : "";
+  const std::string right_device = argc >= 3 ? argv[2] : "/dev/ttyUSB1";
   const int baudrate = argc >= 4 ? std::atoi(argv[3]) : 1000000;
 
   std::cout << "Starting Dynamixel reader on " << left_device << " + "
@@ -16,7 +17,9 @@ int main(int argc, char const* argv[]) {
   while (true) {
     const auto left = reader.left.wait_for_next();
     const auto right = reader.right.wait_for_next();
-    if (!left && !right) break;
+    if (!left && !right){
+      break;
+    }
 
     if (left) {
       std::cout << "L[" << left->timestamp << "]:";
